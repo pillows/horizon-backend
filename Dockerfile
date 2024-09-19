d3a40911d9bb3fd2b130aabc0a4b4d04246274e3
Matthew Wong <matthew.wong@outlook.com>
2024-09-19 00:11:51 -0400

    Add modifications from ec2 deployment

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,16 @@
-FROM python:3.12.6-alpine3.20
-
+FROM python:3.12.6-slim-bullseye
 WORKDIR /app
-
 EXPOSE 4000
 
-COPY . .
+ENV HOST=0.0.0.0
+ENV PORT=4000
 
+RUN apt update -y
+RUN apt install gcc libpq-dev python3-dev -y
+
+COPY . .
 RUN pip install --upgrade pip
 RUN pip install --no-cache-dir -r requirements.txt
 
 RUN chmod +x start.sh
-ENTRYPOINT [ "./start.sh" ]
+ENTRYPOINT ["./start.sh"]
--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,24 @@
-# import os
-# from dotenv import load_dotenv
-# from flask import Flask
-# from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+from flask import Flask
+from flask_cors import CORS
 
-# from app.database import db
-# from app.routes.events import events_bp
-# from app.config import DB_URI
+from app.database import db
+from app.routes.events import events_bp
+from app.config import DB_URI
 
-# load_dotenv()
+load_dotenv()
 
-# app = Flask(__name__)
-# CORS(app)
-# # Config
-# app.env = os.environ.get('ENV')
-# app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+app = Flask(__name__)
+CORS(app)
+# Config
+app.env = os.environ.get('ENV')
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
 
-# db.init_app(app)
+db.init_app(app)
 
-# app.register_blueprint(events_bp, url_prefix='/api/events')
+app.register_blueprint(events_bp, url_prefix='/api/events')
 
-# with app.app_context():
-#     db.create_all()
+with app.app_context():
+    db.create_all()
