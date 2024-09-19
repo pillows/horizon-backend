73e91f985a120e287cd602f695e05065453e775f
Matthew Wong <matthew.wong@outlook.com>
2024-09-19 00:39:47 -0400

    Read env file. For some reason wasn't reading before on prod

--- app/config.py
+++ app/config.py
@@ -1,5 +1,7 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()  # take environment variables
 HOST = os.getenv("HOST", "localhost")
 PORT = int(os.getenv("APPLICATION_PORT", "5000"))
 DB_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///app.db")  # Default to SQLite if not set
